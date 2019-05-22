ddd8f8806fe9ddc463cf581f7b4ff71c25d4508f
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-05-22 18:49:39 +0700

    update thankyou

---
+++ thankyou.php
@@ -0,0 +1,20 @@
+<?php include('header.php'); ?>
+<?php include('breadcrumb2.php'); ?>
+<div class="uk-section-small">
+    <div class="uk-container">
+        <h2 class="title_h uk-text-uppercase uk-text-center uk-margin-medium-bottom">#thank you</h2>
+        <div class="uk-grid uk-flex-center" uk-margin>
+            <div class="uk-width-1-2@m">
+                <div class="uk-card uk-height-1-1 card_sign uk-card-default uk-card-body uk-padding uk-width-1-1">
+                    <div class="uk-text-center"><img src="imgs/checked.png" alt=""></div>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <div class="uk-text-center">
+                        <a href="." class="uk-button uk-button-default uk-margin-small-top uk-first-column">Purchase history</a>
+                        <a href="payment.php" class="uk-button uk-button-secondary uk-margin-small-top uk-first-column">HOME PAGE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('footer.php'); ?>
